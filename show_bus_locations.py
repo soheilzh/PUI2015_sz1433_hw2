eb22ff0daaf53aa3fae74627a1e9d0dbdcde6646
soheilzh <soheill@gmail.com>
2015-09-28 12:51:21 -0400

    Final commit

--- show_bus_locations.py
+++ show_bus_locations.py
@@ -1,25 +1,21 @@
-import json
-import sys
-import csv
+import sys, json, urllib2
 
 if __name__=='__main__':
-    url = 'https://api.prod.obanyc.com/api/siri/vehicle-monitoring.jason?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s' % (sys.argv[1]) % (sys.argv[2])
-    request = urllib2.urlopen(url)
-    metadata = json.loads(request.read())
+
+key = sys.argv[1]
+num = sys.argv[2]
+
+url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key={}&VehicleMonitoringDetailLevel=calls&LineRef={}'.format(key, num)
+
+response = urllib2.urlopen(url)
+info     = json.load( response )
+buses    = info['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+
+print 'Bus Line :', num
+print 'Number of Active Buses :', len(buses)
+for i in range(len(buses)):
+    bus = buses[i]
+    lon = bus['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    lat = bus['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    print 'Bus {} is at latitude {} and longitude {}'.format(i, lat, lon)
     
-    jsonFile = open(sys.argv[1], 'r')
-    data = json.load(jsonFile)
-    stations = data['stationBeanList']
-
-    with open(sys.argv[2], 'w') as csvFile:
-        writer = csv.writer(csvFile)
-        headers = ['Station Name', 'Latitude', 'Longitude']
-        writer.writerow(headers)
-
-        for s in stations:
-            if s['statusKey']!=1 and s['stationName'].startswith('Coming soon'):
-                stationName = s['stationName'][13:]
-                stationLat  = s['latitude']
-                stationLon  = s['longitude']
-                
-                writer.writerow([stationName, stationLat, stationLon])
